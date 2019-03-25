279813de756838a63c7d9adc081afbc326b6a86c
Domenic Denicola <d@domenic.me>
2019-03-24 18:40:51 -0700

    Update dev dependencies

--- test/wpt/XMLSerializer-serializeToString.test.js
+++ test/wpt/XMLSerializer-serializeToString.test.js
@@ -1,10 +1,11 @@
+"use strict";
 const { JSDOM } = require("jsdom");
 
 const XMLSerializer = require("../../lib/XMLSerializer").interface;
 
 describe("WPT", () => {
   const serializer = new XMLSerializer();
-  const DOMParser = new JSDOM().window.DOMParser;
+  const { DOMParser } = (new JSDOM()).window;
 
   function createXmlDoc() {
     const input =
@@ -13,27 +14,27 @@ describe("WPT", () => {
     return parser.parseFromString(input, "text/xml");
   }
 
-  test("check XMLSerializer.serializeToString method could parsing xmldoc to string", async () => {
+  test("check XMLSerializer.serializeToString method could parsing xmldoc to string", () => {
     const document = createXmlDoc();
     expect(serializer.serializeToString(document)).toEqual(
       "<root><child1>value1</child1></root>"
     );
   });
 
-  test("Check if the default namespace is correctly reset.", async () => {
+  test("Check if the default namespace is correctly reset.", () => {
     const document = createXmlDoc();
-    var root = document.documentElement;
-    var element = root.ownerDocument.createElementNS("urn:foo", "another");
-    var child1 = root.firstChild;
+    const root = document.documentElement;
+    const element = root.ownerDocument.createElementNS("urn:foo", "another");
+    const child1 = root.firstChild;
     root.replaceChild(element, child1);
     element.appendChild(child1);
-    var xmlString = serializer.serializeToString(root);
+    const xmlString = serializer.serializeToString(root);
     expect(xmlString).toEqual(
       '<root><another xmlns="urn:foo"><child1 xmlns="">value1</child1></another></root>'
     );
   });
 
-  test("Check if there is no redundant empty namespace declaration.", async () => {
+  test("Check if there is no redundant empty namespace declaration.", () => {
     const input =
       '<root xmlns="urn:bar"><outer xmlns=""><inner>value1</inner></outer></root>';
     const root = new DOMParser().parseFromString(input, "text/xml")
@@ -44,28 +45,21 @@ describe("WPT", () => {
     );
   });
 
-  test("check XMLSerializer.serializeToString escapes attribute values for roundtripping", function() {
-    var serializer = new XMLSerializer();
-    var parser = new DOMParser();
-    var root = parser.parseFromString("<root />", "text/xml").documentElement;
+  test("check XMLSerializer.serializeToString escapes attribute values for roundtripping", () => {
+    const parser = new DOMParser();
+    const root = parser.parseFromString("<root />", "text/xml").documentElement;
 
     root.setAttribute("attr", "\t");
-    expect(['<root attr="&#9;"/>', '<root attr="&#x9;"/>']).toContain(
-      serializer.serializeToString(root)
-    );
+    expect(['<root attr="&#9;"/>', '<root attr="&#x9;"/>']).toContain(serializer.serializeToString(root));
 
     root.setAttribute("attr", "\n");
-    expect(['<root attr="&#xA;"/>', '<root attr="&#10;"/>']).toContain(
-      serializer.serializeToString(root)
-    );
+    expect(['<root attr="&#xA;"/>', '<root attr="&#10;"/>']).toContain(serializer.serializeToString(root));
 
     root.setAttribute("attr", "\r");
-    expect(['<root attr="&#xD;"/>', '<root attr="&#13;"/>']).toContain(
-      serializer.serializeToString(root)
-    );
+    expect(['<root attr="&#xD;"/>', '<root attr="&#13;"/>']).toContain(serializer.serializeToString(root));
   });
 
-  test("Check if unknown prefixes are handled correctly", function() {
+  test("Check if unknown prefixes are handled correctly", () => {
     const document = createXmlDoc();
     const root = document.documentElement;
     const element = root.ownerDocument.createElementNS(
--- test/wpt/xml-serialization.test.js
+++ test/wpt/xml-serialization.test.js
@@ -1,93 +1,96 @@
+"use strict";
 const { JSDOM } = require("jsdom");
 
 const XMLSerializer = require("../../lib/XMLSerializer").interface;
 
 function serialize(node) {
-  var serializer = new XMLSerializer();
+  const serializer = new XMLSerializer();
   return serializer.serializeToString(node);
 }
 
 describe("xml-serialization", () => {
-  const document = new JSDOM().window.document;
+  const { document } = (new JSDOM()).window;
 
-  test("Comment: containing --", function() {
-    var dt = document.createComment("--");
+  test("Comment: containing --", () => {
+    const dt = document.createComment("--");
     expect(serialize(dt)).toEqual("<!------>");
   });
-  test("Comment: starting with -", function() {
-    var dt = document.createComment("- x");
+  test("Comment: starting with -", () => {
+    const dt = document.createComment("- x");
     expect(serialize(dt)).toEqual("<!--- x-->");
   });
-  test("Comment: ending with -", function() {
-    var dt = document.createComment("x -");
+  test("Comment: ending with -", () => {
+    const dt = document.createComment("x -");
     expect(serialize(dt)).toEqual("<!--x --->");
   });
-  test("Comment: containing -->", function() {
-    var dt = document.createComment("-->");
+  test("Comment: containing -->", () => {
+    const dt = document.createComment("-->");
     expect(serialize(dt)).toEqual("<!---->-->");
   });
-  test("DocumentType: empty public and system id", function() {
-    var dt = document.implementation.createDocumentType("html", "", "");
+  test("DocumentType: empty public and system id", () => {
+    const dt = document.implementation.createDocumentType("html", "", "");
     expect(serialize(dt)).toEqual("<!DOCTYPE html>");
   });
-  test("DocumentType: empty system id", function() {
-    var dt = document.implementation.createDocumentType("html", "a", "");
+  test("DocumentType: empty system id", () => {
+    const dt = document.implementation.createDocumentType("html", "a", "");
     expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "a">');
   });
-  test("DocumentType: empty public id", function() {
-    var dt = document.implementation.createDocumentType("html", "", "a");
+  test("DocumentType: empty public id", () => {
+    const dt = document.implementation.createDocumentType("html", "", "a");
     expect(serialize(dt)).toEqual('<!DOCTYPE html SYSTEM "a">');
   });
-  test("DocumentType: non-empty public and system id", function() {
-    var dt = document.implementation.createDocumentType("html", "a", "b");
+  test("DocumentType: non-empty public and system id", () => {
+    const dt = document.implementation.createDocumentType("html", "a", "b");
     expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "a" "b">');
   });
-  test("DocumentType: 'APOSTROPHE' (U+0027)", function() {
-    var dt = document.implementation.createDocumentType("html", "'", "'");
+  test("DocumentType: 'APOSTROPHE' (U+0027)", () => {
+    const dt = document.implementation.createDocumentType("html", "'", "'");
     expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "\'" "\'">');
   });
-  test("DocumentType: 'QUOTATION MARK' (U+0022)", function() {
-    var dt = document.implementation.createDocumentType("html", '"', '"');
+  test("DocumentType: 'QUOTATION MARK' (U+0022)", () => {
+    const dt = document.implementation.createDocumentType("html", '"', '"');
     expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC """ """>');
   });
-  test("DocumentType: 'APOSTROPHE' (U+0027) and 'QUOTATION MARK' (U+0022)", function() {
-    var dt = document.implementation.createDocumentType("html", "\"'", "'\"");
+  test("DocumentType: 'APOSTROPHE' (U+0027) and 'QUOTATION MARK' (U+0022)", () => {
+    const dt = document.implementation.createDocumentType("html", "\"'", "'\"");
     expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC ""\'" "\'"">');
   });
-  test("Element: href attributes are not percent-encoded", function() {
-    var el = document.createElement("a");
+  test("Element: href attributes are not percent-encoded", () => {
+    const el = document.createElement("a");
     el.setAttribute(
       "href",
-      "\u3042\u3044\u3046 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+      "\u3042\u3044\u3046 !\"#$%&'()*+,-./0123456789:;<=>?@A" +
+      "BCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
     );
     expect(serialize(el)).toEqual(
       '<a xmlns="http://www.w3.org/1999/xhtml" href="\u3042\u3044\u3046 !&quot;#$%&amp;\'()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"></a>'
     );
   });
-  test("Element: query parts in href attributes are not percent-encoded", function() {
-    var el = document.createElement("a");
+  test("Element: query parts in href attributes are not percent-encoded", () => {
+    const el = document.createElement("a");
     el.setAttribute(
       "href",
-      "?\u3042\u3044\u3046 !\"$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+      "?\u3042\u3044\u3046 !\"$%&'()*+,-./0123456789:;<=>?@" +
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
     );
     expect(serialize(el)).toEqual(
       '<a xmlns="http://www.w3.org/1999/xhtml" href="?\u3042\u3044\u3046 !&quot;$%&amp;\'()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"></a>'
     );
   });
-  test("ProcessingInstruction: empty data", function() {
-    var pi = document.createProcessingInstruction("a", "");
+  test("ProcessingInstruction: empty data", () => {
+    const pi = document.createProcessingInstruction("a", "");
     expect(serialize(pi)).toEqual("<?a ?>");
   });
-  test("ProcessingInstruction: non-empty data", function() {
-    var pi = document.createProcessingInstruction("a", "b");
+  test("ProcessingInstruction: non-empty data", () => {
+    const pi = document.createProcessingInstruction("a", "b");
     expect(serialize(pi)).toEqual("<?a b?>");
   });
-  test("ProcessingInstruction: target contains xml", function() {
-    var pi = document.createProcessingInstruction("xml", "b");
+  test("ProcessingInstruction: target contains xml", () => {
+    const pi = document.createProcessingInstruction("xml", "b");
     expect(serialize(pi)).toEqual("<?xml b?>");
   });
-  test("ProcessingInstruction: target contains a 'COLON' (U+003A)", function() {
-    var pi = document.createProcessingInstruction("x:y", "b");
+  test("ProcessingInstruction: target contains a 'COLON' (U+003A)", () => {
+    const pi = document.createProcessingInstruction("x:y", "b");
     expect(serialize(pi)).toEqual("<?x:y b?>");
   });
 });
