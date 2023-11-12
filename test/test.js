02e7df6c9f9426598c76c1ac8eb04002b5282be4
Domenic Denicola <d@domenic.me>
2023-11-12 16:58:02 +0900

    Update dependencies and move from yarn to npm
    Notably, we can remove jest in favor of Node.js's built-in test runner.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 "use strict";
+const { describe, test } = require("node:test");
+const assert = require("node:assert/strict");
 const { JSDOM } = require("jsdom");
 const serialize = require("..");
 
@@ -7,47 +9,47 @@ describe("Derived from WPT xml-serialization.xhtml", () => {
 
   test("Comment: containing --", () => {
     const dt = document.createComment("--");
-    expect(serialize(dt)).toEqual("<!------>");
+    assert.equal(serialize(dt), "<!------>");
   });
   test("Comment: starting with -", () => {
     const dt = document.createComment("- x");
-    expect(serialize(dt)).toEqual("<!--- x-->");
+    assert.equal(serialize(dt), "<!--- x-->");
   });
   test("Comment: ending with -", () => {
     const dt = document.createComment("x -");
-    expect(serialize(dt)).toEqual("<!--x --->");
+    assert.equal(serialize(dt), "<!--x --->");
   });
   test("Comment: containing -->", () => {
     const dt = document.createComment("-->");
-    expect(serialize(dt)).toEqual("<!---->-->");
+    assert.equal(serialize(dt), "<!---->-->");
   });
   test("DocumentType: empty public and system id", () => {
     const dt = document.implementation.createDocumentType("html", "", "");
-    expect(serialize(dt)).toEqual("<!DOCTYPE html>");
+    assert.equal(serialize(dt), "<!DOCTYPE html>");
   });
   test("DocumentType: empty system id", () => {
     const dt = document.implementation.createDocumentType("html", "a", "");
-    expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "a">');
+    assert.equal(serialize(dt), '<!DOCTYPE html PUBLIC "a">');
   });
   test("DocumentType: empty public id", () => {
     const dt = document.implementation.createDocumentType("html", "", "a");
-    expect(serialize(dt)).toEqual('<!DOCTYPE html SYSTEM "a">');
+    assert.equal(serialize(dt), '<!DOCTYPE html SYSTEM "a">');
   });
   test("DocumentType: non-empty public and system id", () => {
     const dt = document.implementation.createDocumentType("html", "a", "b");
-    expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "a" "b">');
+    assert.equal(serialize(dt), '<!DOCTYPE html PUBLIC "a" "b">');
   });
   test("DocumentType: 'APOSTROPHE' (U+0027)", () => {
     const dt = document.implementation.createDocumentType("html", "'", "'");
-    expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC "\'" "\'">');
+    assert.equal(serialize(dt), '<!DOCTYPE html PUBLIC "\'" "\'">');
   });
   test("DocumentType: 'QUOTATION MARK' (U+0022)", () => {
     const dt = document.implementation.createDocumentType("html", '"', '"');
-    expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC """ """>');
+    assert.equal(serialize(dt), '<!DOCTYPE html PUBLIC """ """>');
   });
   test("DocumentType: 'APOSTROPHE' (U+0027) and 'QUOTATION MARK' (U+0022)", () => {
     const dt = document.implementation.createDocumentType("html", "\"'", "'\"");
-    expect(serialize(dt)).toEqual('<!DOCTYPE html PUBLIC ""\'" "\'"">');
+    assert.equal(serialize(dt), '<!DOCTYPE html PUBLIC ""\'" "\'"">');
   });
   test("Element: href attributes are not percent-encoded", () => {
     const el = document.createElement("a");
@@ -56,7 +58,8 @@ describe("Derived from WPT xml-serialization.xhtml", () => {
       "\u3042\u3044\u3046 !\"#$%&'()*+,-./0123456789:;<=>?@A" +
       "BCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
     );
-    expect(serialize(el)).toEqual(
+    assert.equal(
+      serialize(el),
       '<a xmlns="http://www.w3.org/1999/xhtml" href="\u3042\u3044\u3046 !&quot;#$%&amp;\'()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"></a>'
     );
   });
@@ -67,25 +70,26 @@ describe("Derived from WPT xml-serialization.xhtml", () => {
       "?\u3042\u3044\u3046 !\"$%&'()*+,-./0123456789:;<=>?@" +
       "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
     );
-    expect(serialize(el)).toEqual(
+    assert.equal(
+      serialize(el),
       '<a xmlns="http://www.w3.org/1999/xhtml" href="?\u3042\u3044\u3046 !&quot;$%&amp;\'()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"></a>'
     );
   });
   test("ProcessingInstruction: empty data", () => {
     const pi = document.createProcessingInstruction("a", "");
-    expect(serialize(pi)).toEqual("<?a ?>");
+    assert.equal(serialize(pi), "<?a ?>");
   });
   test("ProcessingInstruction: non-empty data", () => {
     const pi = document.createProcessingInstruction("a", "b");
-    expect(serialize(pi)).toEqual("<?a b?>");
+    assert.equal(serialize(pi), "<?a b?>");
   });
   test("ProcessingInstruction: target contains xml", () => {
     const pi = document.createProcessingInstruction("xml", "b");
-    expect(serialize(pi)).toEqual("<?xml b?>");
+    assert.equal(serialize(pi), "<?xml b?>");
   });
   test("ProcessingInstruction: target contains a 'COLON' (U+003A)", () => {
     const pi = document.createProcessingInstruction("x:y", "b");
-    expect(serialize(pi)).toEqual("<?x:y b?>");
+    assert.equal(serialize(pi), "<?x:y b?>");
   });
 });
 
@@ -101,7 +105,8 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
 
   test("check XMLSerializer.serializeToString method could parsing xmldoc to string", () => {
     const document = createXMLDoc();
-    expect(serialize(document)).toEqual(
+    assert.equal(
+      serialize(document),
       "<root><child1>value1</child1></root>"
     );
   });
@@ -114,7 +119,8 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
     root.replaceChild(element, child1);
     element.appendChild(child1);
     const xmlString = serialize(root);
-    expect(xmlString).toEqual(
+    assert.equal(
+      xmlString,
       '<root><another xmlns="urn:foo"><child1 xmlns="">value1</child1></another></root>'
     );
   });
@@ -125,7 +131,8 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
     const root = new DOMParser().parseFromString(input, "text/xml")
       .documentElement;
 
-    expect(serialize(root)).toEqual(
+    assert.equal(
+      serialize(root),
       '<root xmlns="urn:bar"><outer xmlns=""><inner>value1</inner></outer></root>'
     );
   });
@@ -135,13 +142,13 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
     const root = parser.parseFromString("<root />", "text/xml").documentElement;
 
     root.setAttribute("attr", "\t");
-    expect(['<root attr="&#9;"/>', '<root attr="&#x9;"/>']).toContain(serialize(root));
+    assert.equal(serialize(root), '<root attr="&#x9;"/>');
 
     root.setAttribute("attr", "\n");
-    expect(['<root attr="&#xA;"/>', '<root attr="&#10;"/>']).toContain(serialize(root));
+    assert.equal(serialize(root), '<root attr="&#xA;"/>');
 
     root.setAttribute("attr", "\r");
-    expect(['<root attr="&#xD;"/>', '<root attr="&#13;"/>']).toContain(serialize(root));
+    assert.equal(serialize(root), '<root attr="&#xD;"/>');
   });
 
   test("Check if unknown prefixes are handled correctly", () => {
@@ -152,7 +159,8 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
       "html:br"
     );
     root.appendChild(element);
-    expect(serialize(root, { requireWellFormed: true })).toEqual(
+    assert.equal(
+      serialize(root, { requireWellFormed: true }),
       '<root><child1>value1</child1><html:br xmlns:html="http://www.w3.org/1999/xhtml" /></root>'
     );
   });
@@ -163,7 +171,7 @@ describe("Derived from WPT XMLSerializer-serializeToString.html", () => {
 
     const document = new DOMParser().parseFromString(markup, "application/xml");
 
-    expect(serialize(document)).toEqual(markup);
+    assert.equal(serialize(document), markup);
   });
 });
 
@@ -174,27 +182,26 @@ describe("Well-formedness test cases", () => {
 
   // Derived from https://www.w3.org/TR/xml/#NT-Char
   test("Check Characters are in range", () => {
-    expect(() => {
-      const emojiNode = document.createTextNode("Emoji 🔍");
-      serialize(emojiNode, { requireWellFormed: true });
-    }).not.toThrow();
+    // Test this does not throw:
+    const emojiNode = document.createTextNode("Emoji 🔍");
+    serialize(emojiNode, { requireWellFormed: true });
 
     const expectedError = new Error("Failed to serialize XML: text node data is not well-formed.");
 
-    expect(() => {
+    assert.throws(() => {
       const surrogateBlockNode = document.createTextNode("Surrogate block \uD800");
       serialize(surrogateBlockNode, { requireWellFormed: true });
-    }).toThrow(expectedError);
+    }, expectedError);
 
-    expect(() => {
+    assert.throws(() => {
       const fffeNode = document.createTextNode("FFFE \uFFFE");
       serialize(fffeNode, { requireWellFormed: true });
-    }).toThrow(expectedError);
+    }, expectedError);
 
-    expect(() => {
+    assert.throws(() => {
       const ffffNode = document.createTextNode("FFFF \uFFFF");
       serialize(ffffNode, { requireWellFormed: true });
-    }).toThrow(expectedError);
+    }, expectedError);
   });
 });
 
@@ -212,7 +219,8 @@ describe("Our own test cases", () => {
     root.setAttributeNS("https://example.com/", "attribute1", "value");
     root.setAttributeNS("https://example.com/", "attribute2", "value");
 
-    expect(serialize(document)).toEqual(
+    assert.equal(
+      serialize(document),
       '<root xmlns:ns1="https://example.com/" ns1:attribute1="value" ns1:attribute2="value"><child1>value1</child1></root>'
     );
   });
@@ -222,11 +230,12 @@ describe("Our own test cases", () => {
 
     const els = document.getElementsByTagName("element");
 
-    expect(els).toHaveLength(1);
-    expect(els[0].attributes).toHaveLength(2);
-    expect(els[0].attributes[1].prefix).toEqual("prefix");
-    expect(els[0].getAttribute("prefix:hasOwnProperty")).toEqual("value");
-    expect(serialize(els[0])).toEqual(
+    assert.equal(els.length, 1);
+    assert.equal(els[0].attributes.length, 2);
+    assert.equal(els[0].attributes[1].prefix, "prefix");
+    assert.equal(els[0].getAttribute("prefix:hasOwnProperty"), "value");
+    assert.equal(
+      serialize(els[0]),
       `<element xmlns:prefix="https://example.com/" prefix:hasOwnProperty="value"/>`
     );
   });
@@ -237,6 +246,6 @@ describe("Our own test cases", () => {
     const el = document.createElement("el");
     el.appendChild(document.createTextNode("\t"));
 
-    expect(serialize(el, { requireWellFormed: true })).toEqual("<el>\t</el>");
+    assert.equal(serialize(el, { requireWellFormed: true }), "<el>\t</el>");
   });
 });
